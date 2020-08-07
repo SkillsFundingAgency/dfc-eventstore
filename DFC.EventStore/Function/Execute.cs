5b862698510e0743c2fcd1f56f2912fa35e9721d
Tom Tollafield <thomas.tollafield@gmail.com>
2020-08-07 13:30:21 +0100

    - Change to upsert - Use Cosmos Package

--- DFC.EventStore/Function/Execute.cs
+++ DFC.EventStore/Function/Execute.cs
@@ -2,17 +2,17 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
-using DFC.Eventstore.Data.Contracts;
 using DFC.App.EventStore.Data.Models;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using DFC.Compui.Cosmos.Contracts;
 
 namespace DFC.EventStore.Function
 {
     public class Execute
     {
-        private readonly ICosmosRepository<EventStoreModel> _eventstoreRepository;
+        private readonly IDocumentService<EventStoreModel> _eventstoreRepository;
 
-        public Execute(ICosmosRepository<EventStoreModel> eventstoreRepository)
+        public Execute(IDocumentService<EventStoreModel> eventstoreRepository)
         {
             _eventstoreRepository = eventstoreRepository ?? throw new ArgumentNullException(nameof(eventstoreRepository));
         }
@@ -26,7 +26,8 @@ namespace DFC.EventStore.Function
                 if (eventGridEvent != null)
                 {
                     log.LogInformation($"Request received: {eventGridEvent}");
-                    await _eventstoreRepository.CreateAsync(eventGridEvent);
+
+                    await _eventstoreRepository.UpsertAsync(eventGridEvent);
                 }
             }
 
--- DFC.Eventstore.Data/Models/EventStoreModel.cs
+++ DFC.Eventstore.Data/Models/EventStoreModel.cs
@@ -1,9 +1,31 @@
-﻿using DFC.Eventstore.Data.Models;
+﻿using DFC.Compui.Cosmos.Contracts;
+using DFC.Compui.Telemetry.Models;
+using DFC.Eventstore.Data.Models;
 using Microsoft.Azure.EventGrid.Models;
+using Newtonsoft.Json;
+using System;
 
 namespace DFC.App.EventStore.Data.Models
 {
-    public class EventStoreModel : EventGridEvent, IDataModel
+    public class EventStoreModel : RequestTrace, IDocumentModel
     {
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+        [JsonProperty(PropertyName = "topic")]
+        public string? Topic { get; set; }
+        [JsonProperty(PropertyName = "subject")]
+        public string? Subject { get; set; }
+        [JsonProperty(PropertyName = "data")]
+        public object? Data { get; set; }
+        [JsonProperty(PropertyName = "eventType")]
+        public string? EventType { get; set; }
+        [JsonProperty(PropertyName = "eventTime")]
+        public DateTime? EventTime { get; set; }
+        [JsonProperty(PropertyName = "metadataVersion")]
+        public string? MetadataVersion { get; }
+        [JsonProperty(PropertyName = "dataVersion")]
+        public string? DataVersion { get; set; }
+        public string? Etag { get; set; }
+        public string? PartitionKey { get; set; }
     }
 }
