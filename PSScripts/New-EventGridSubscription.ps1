7eee0dbccef41208da73428f9cc835a6904142ad
olusola-adio <olusola_adio@hotmail.com>
2020-10-22 05:54:57 +0100

    NCSD-4605-eventgrid-subscription

--- PSScripts/New-EventGridSubscription.ps1
+++ PSScripts/New-EventGridSubscription.ps1
@@ -3,25 +3,25 @@
 Creates EventGrid Subscription.  
 
 .DESCRIPTION
-Creates EventGrid Subscription.  
+Creates EventGrid Subscription.  The topic and function app must already exist along with their resourcegroups
 
 .PARAMETER EventGridSubscriptionName
 The EventGrid Subscription Name
 
 .PARAMETER SubscriptionId
-The Subscription Id
+The Azure Subscription Id
 
 .PARAMETER TopicResourceGroup
 The Topic ResourceGroup
 
 .PARAMETER Topic
-The Topic
+The name of the Topic
 
 .PARAMETER FunctionAppResourceGroup
 The Function App ResourceGroup
 
 .PARAMETER FunctionApp
-The Function App
+The name of the Function App
 #>
 
 [CmdletBinding()]
@@ -40,9 +40,10 @@ param(
     [String]$FunctionApp
 )
 
-
+# this is to ensure that the latest extension of AZ CLI eventgrid has been installed
 az extension add --name eventgrid
 
+# Create the event grid subscriptionfrom an existing custom topic
 az eventgrid event-subscription create `
 --name $EventGridSubscriptionName `
 --source-resource-id "/subscriptions/$($SubscriptionId)/resourceGroups/$($TopicResourceGroup)/providers/Microsoft.EventGrid/topics/$($Topic)" `
