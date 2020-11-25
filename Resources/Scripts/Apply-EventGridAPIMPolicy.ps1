33ace47ee98238367d2042bc086134fb48799c21
olusola-adio <olusola_adio@hotmail.com>
2020-11-25 13:36:24 +0000

    NCSD-4777-added PS script to register policy

---
+++ Resources/Scripts/Apply-EventGridAPIMPolicy.ps1
@@ -0,0 +1,64 @@
+
+<#
+.SYNOPSIS
+Update an APIM API with an openapi definition
+
+.DESCRIPTION
+Update an APIM API with a openapi definition
+
+.PARAMETER ApimResourceGroup
+The name of the resource group that contains the APIM instnace
+
+.PARAMETER ApimServiceName
+The name of the APIM instance
+
+.PARAMETER ApiNames
+The name of the API to update
+
+.PARAMETER policyFilePath
+The path to the policy file.
+
+.EXAMPLE
+Import-ApimOpenApiDefinitionFromFile -ApimResourceGroup dfc-foo-bar-rg -InstanceName dfc-foo-bar-apim -ApiName bar -OpenApiSpecificationFile some-file.yaml -Verbose
+
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [String]$ApimResourceGroup,
+    [Parameter(Mandatory=$true)]
+    [String]$ApimServiceName,
+    [Parameter(Mandatory=$true)]
+    [String]$ApiNames,
+    [Parameter(Mandatory=$true)]
+    [String]$policyFilePath
+)
+
+try {
+    $context = New-AzApiManagementContext -ResourceGroupName $ApimResourceGroup -ServiceName $ApimServiceName
+
+    $api = Get-AzApiManagementApi -Context $context -Name $ApiNames
+    
+    $apiOperations = Get-AzApiManagementOperation -Context $context -ApiId $APIId
+    
+    $apiOperationNames = $apiOperations.Name
+    
+    foreach ($apiName in $ApiNames) {
+        $api = ""
+    
+        $api = Get-AzApiManagementApi -Context $context -Name $apiName
+    
+        foreach ($apiOperationName in $apiOperationNames) {
+            $operation = ""
+    
+            $operation = Get-AzApiManagementOperation -Context $context -ApiId $api.ApiId | Where-Object {$_.Name -eq $apiOperationName}
+    
+            Write-Output "Applying policy to API: $($api.name) Operation: $($operation.name)"
+    
+            Set-AzApiManagementPolicy -Context $context -Format application/vnd.ms-azure-apim.policy.raw+xml -ApiId $api.ApiId -OperationId $operation.OperationId -PolicyFilePath $PolicyFilePath -Verbose
+        }
+    }   
+}
+catch {
+    throw $_    
+}
