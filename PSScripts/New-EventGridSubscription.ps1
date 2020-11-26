16d8cc2281c6323295b4f85715e3d88258aaefe9
OLUSOLA ADIO <61968373+olusola-adio@users.noreply.github.com>
2020-11-26 07:55:45 +0000

    Ncsd 4777 http trigger subscription (#20)
    * NCSD-4777-http-trigger-subscription-changes
    
    * NCSD-4777-added-apim-stuff-for-subscription
    
    * NCSD-4777-fix
    
    * NCSD-4777-fix
    
    * NCSD-4777-fix
    
    * NCSD-4777-added PS script to register policy
    
    * NCSD-4777-fix
    
    * NCSD-4777-changed-EndpointType-to-webhook
    
    * NCSD-4777-fixed-typo

--- PSScripts/New-EventGridSubscription.ps1
+++ PSScripts/New-EventGridSubscription.ps1
@@ -8,20 +8,14 @@ Creates EventGrid Subscription.  The topic and function app must already exist a
 .PARAMETER EventGridSubscriptionName
 The EventGrid Subscription Name
 
-.PARAMETER SubscriptionId
-The Azure Subscription Id
-
 .PARAMETER TopicResourceGroup
 The Topic ResourceGroup
 
 .PARAMETER Topic
 The name of the Topic
 
-.PARAMETER FunctionAppResourceGroup
-The Function App ResourceGroup
-
-.PARAMETER FunctionApp
-The name of the Function App
+.PARAMETER SubscriptionEndPoint
+The name of theSubscription EndPoint
 #>
 
 [CmdletBinding()]
@@ -29,24 +23,17 @@ param(
     [Parameter(Mandatory=$true)]
     [String]$EventGridSubscriptionName,
     [Parameter(Mandatory=$true)]
-    [String]$SubscriptionId,
-    [Parameter(Mandatory=$true)]
     [String]$TopicResourceGroup,
     [Parameter(Mandatory=$true)]
     [String]$Topic,
     [Parameter(Mandatory=$true)]
-    [String]$FunctionAppResourceGroup,
-    [Parameter(Mandatory=$true)]
-    [String]$FunctionApp
+    [String]$SubscriptionEndPoint
 )
 
-# this is to ensure that the latest extension of AZ CLI eventgrid has been installed
-az extension add --name eventgrid
-
-# Create the event grid subscriptionfrom an existing custom topic
-az eventgrid event-subscription create `
---name $EventGridSubscriptionName `
---source-resource-id "/subscriptions/$($SubscriptionId)/resourceGroups/$($TopicResourceGroup)/providers/Microsoft.EventGrid/topics/$($Topic)" `
---endpoint "/subscriptions/$($SubscriptionId)/resourceGroups/$($FunctionAppResourceGroup)/providers/Microsoft.Web/sites/$($FunctionApp)/functions/Execute" `
---endpoint-type azurefunction
-
+New-AzEventGridSubscription `
+    -EventSubscriptionName  $EventGridSubscriptionName `
+    -ResourceGroupName $TopicResourceGroup `
+    -TopicName $Topic `
+    -EndpointType 'webhook' `
+    -Endpoint  $SubscriptionEndPoint `
+    -Verbose
